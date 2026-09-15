5b2531405796d3d04570623f81c4fe3b40958d18
Juez34 <jerome.renon@gmail.com>
2026-09-15 16:03:21 +0200

    Update page.js

--- app/coach/history/program/[id]/page.js
+++ app/coach/history/program/[id]/page.js
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState, useEffect } from "react";
-import { supabase } from "../../../../lib/supabase";
+import { supabase } from "../../../../../lib/supabase";
 import { ArrowLeft, Loader2, Dumbbell, Calendar, Clock, Check, X, CheckCircle2, Home } from "lucide-react";
 import Link from "next/link";
 import { useParams, useSearchParams, useRouter } from "next/navigation";
