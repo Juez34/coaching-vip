2aced5584859d984d6755cceff62bbe32e4c6ff6
Juez34 <jerome.renon@gmail.com>
2026-09-16 16:37:05 +0200

    Update page.js

--- app/coach/history/program/[id]/page.js
+++ app/coach/history/program/[id]/page.js
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState, useEffect } from "react";
-import { supabase } from "../="../../../../lib/supabase";
+import { supabase } from "../="../../../../../lib/supabase";
 import { 
   ArrowLeft, Loader2, Clock, Dumbbell, 
   ChevronDown, ChevronUp, CheckCircle2, AlertCircle, MessageSquare, Check 
